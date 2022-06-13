e02a6d0de9c24313254932568d7cd6824fee28c7
Mayara Rocha <mayaraaraujorocha54@gmail.com>
2022-06-13 19:49:47 -0300

    projeto finalizado

---
+++ script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const images = [
+  {'id': '1', 'url': './img/apenas.jpg'},
+  {'id': '2', 'url': './img/aventura.jpg'},
+  {'id': '3', 'url': './img/coragem.jpg'},
+  {'id': '4', 'url': './img/gunball.jpg'},
+  {'id': '5', 'url': './img/jorel.jpg'},
+  {'id': '6', 'url': './img/steven.jpg'},
+]
+const containerItems = document.querySelector('#container-items');
+
+const loadImages = ( images, container ) => {
+ images.forEach( image => {
+  container.innerHTML += `
+   <div class='item'>
+   <img src='${image.url}'
+   </div>
+  `
+ })
+}
+
+loadImages( images, containerItems );
+
+let items =  document.querySelectorAll('.item');
+
+const previous = () => {
+  containerItems.appendChild(items[0]);
+  items = document.querySelectorAll('.item')
+}
+const next = () => {
+  const lastItem= items[items.length - 1];
+  containerItems.insertBefore( lastItem, items[0]);
+  items =  document.querySelectorAll('.item')
+}
+
+document.querySelector('#previous').addEventListener('click', previous);
+document.querySelector('#next').addEventListener('click', next);
